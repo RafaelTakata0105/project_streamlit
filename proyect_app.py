eda7ce00271f9126b615f54eda573b6543fc09e3
RafaelTakata0105 <rafaeltakata0105@gmail.com>
2024-04-28 15:44:54 -0600

    porclass

--- proyect_app.py
+++ proyect_app.py
@@ -10,3 +10,6 @@ st.subheader('Primavera 2024')
 st.markdown('La escuela es una etapa de formación en la que desarrollamos habilidades y conocimientos como ninguna otra. Comprender los factores que afectan al rendimiento de los alumnos nos ayudaría a crear un sistema educativo capaz de maximizar el aprendizaje y aprovechar realmente la educación recibida.')
 st.markdown('En 2008, Paulo Cortez y Alice Silva se dedicaron a recoger el mayor número de características de algunos estudiantes de secundaria en el país de Portugal. Estudiaron dos grupos, los que estudiaban portugués y los que estudiaban matemáticas. En este proyecto, trabajaremos con el primero para intentar desarrollar un modelo de predicción para el segundo.')
 st.markdown("La creación de un modelo capaz de describir esta relación nos informaría sobre las áreas de oportunidad dentro del sistema educativo de la nación, además de aportar datos significativos sobre la juventud del país.")
+
+porclass_df = pd.read_csv('student-por.csv')
+st.dataframe(porclass_df)
