18ee5687f7f37793ffbacd18783fc4b661f3ce22
RafaelTakata0105 <rafaeltakata0105@gmail.com>
2024-04-28 15:58:57 -0600

    multiselector

--- proyect_app.py
+++ proyect_app.py
@@ -11,10 +11,13 @@ st.markdown('La escuela es una etapa de formación en la que desarrollamos habil
 st.markdown('En 2008, Paulo Cortez y Alice Silva se dedicaron a recoger el mayor número de características de algunos estudiantes de secundaria en el país de Portugal. Estudiaron dos grupos, los que estudiaban portugués y los que estudiaban matemáticas. En este proyecto, trabajaremos con el primero para intentar desarrollar un modelo de predicción para el segundo.')
 st.markdown("La creación de un modelo capaz de describir esta relación nos informaría sobre las áreas de oportunidad dentro del sistema educativo de la nación, además de aportar datos significativos sobre la juventud del país.")
 
-st.subheader('Clase de portugues:')
+st.subheader('Clase de portugués:')
 porclass_df = pd.read_csv('student-por.csv')
 st.dataframe(porclass_df)
 
 st.subheader('Clase de Matemáticas:')
 math_df = pd.read_csv('student-mat.csv')
 st.dataframe(math_df)
+
+st.subheader('Make your own graphs:')
+st.multiselect('Select', porclass_df.columns)
