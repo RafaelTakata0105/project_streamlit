cec2908c570ff26ae4f84b52b1fb9622681c44ba
RafaelTakata0105 <rafaeltakata0105@gmail.com>
2024-04-28 15:36:31 -0600

    first text

--- proyect_app.py
+++ proyect_app.py
@@ -7,3 +7,4 @@ st.title('Proyecto de analisis de Programación de datos')
 st.header('Rafael Takata Garcia')
 st.subheader('Primavera 2024')
 
+st.markdown('School is a stage of formation in which we develop skills and knowledge like no other. Understanding the factors that affect student performance would help us create an educational system capable of maximizing learning and really take advantage of the education received.')
